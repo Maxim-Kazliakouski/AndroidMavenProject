ee61d01bd9e02d5922f06c33cfe2430abdff9994
Maxim-Kazliakouski <maxim.kazliakouski@gmail.com>
2023-02-28 14:18:04 +0300

    adding BaseTest class

--- src/test/java/tests/base/BaseTest.java
+++ src/test/java/tests/base/BaseTest.java
@@ -51,43 +51,19 @@ public abstract class BaseTest {
     @BeforeMethod
     public void setup() throws MalformedURLException {
         //добавляем логирование действий для аллюр отчета в виде степов
-//        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
 //        // папка для сохранения скриншотов selenide
-//        Configuration.reportsFolder = SCREENSHOT_TO_SAVE_FOLDER;
-//        //инициализируем андройд драйвер
-//        Configuration.browser = runHelper().getDriverClass().getName();
-//        Configuration.startMaximized = false;
-//        Configuration.browserSize = null;
-//        Configuration.timeout = 5000;
-//        disableAnimationOnEmulator();
+        Configuration.reportsFolder = SCREENSHOT_TO_SAVE_FOLDER;
+        //инициализируем андройд драйвер
+        Configuration.browser = runHelper().getDriverClass().getName();
+        Configuration.startMaximized = false;
+        Configuration.browserSize = null;
+        Configuration.timeout = 5000;
+        disableAnimationOnEmulator();
         // the list of pages with steps
         loginPageSteps = new LoginPageSteps();
         mainPageSteps = new MainPageSteps();
         DesiredCapabilities capabilities = new DesiredCapabilities();
-
-        capabilities.setCapability(AndroidMobileCapabilityType.AUTO_GRANT_PERMISSIONS, true);
-        capabilities.setCapability("BROWSER_NAME", "Android");
-        capabilities.setCapability("VERSION", "10");
-        capabilities.setCapability("deviceName", "nexus_5");
-        capabilities.setCapability("platformName", "Android");
-        capabilities.setCapability("appWaitActivity", "*");
-        capabilities.setCapability("appPackage", APP_PACKAGE);
-        capabilities.setCapability("appActivity", APP_ACTIVITY);
-        capabilities.setCapability("app", "/opt/SauceLabs.apk");
-
-
-//        capabilities.setCapability("BROWSER_NAME", "Android");
-//        capabilities.setCapability("VERSION", "10.0");
-//        capabilities.setCapability("deviceName", "Nexus5");
-//        capabilities.setCapability("platformName", "Android");
-
-
-//        capabilities.setCapability("appPackage", "com.android.calculator2");
-// This package name of your app (you can get it from apk info app)
-//        capabilities.setCapability("appActivity","com.android.calculator2.Calculator"); // This is Launcher activity of your app (you can get it from apk info app)
-//Create RemoteWebDriver instance and connect to the Appium server
-        //It will launch the Calculator App in Android Device using the configurations specified in Desired Capabilities
-        driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), capabilities);
     }
 
 
@@ -98,30 +74,30 @@ public abstract class BaseTest {
  * <p>
  * Проверка скриншота с эталоном для проверки верстки
  *
- * @param actualScreenshot актуальный скриншот
- * @param expectedFileName название файла для сравнений
+// * @param actualScreenshot актуальный скриншот
+// * @param expectedFileName название файла для сравнений
  */
-//    private static void disableAnimationOnEmulator() {
-//        executeBash("adb -s shell settings put global transition_animation_scale 0.0");
-//        executeBash("adb -s shell settings put global window_animation_scale 0.0");
-//        executeBash("adb -s shell settings put global animator_duration_scale 0.0");
-//    }
+    private static void disableAnimationOnEmulator() {
+        executeBash("adb -s shell settings put global transition_animation_scale 0.0");
+        executeBash("adb -s shell settings put global window_animation_scale 0.0");
+        executeBash("adb -s shell settings put global animator_duration_scale 0.0");
+    }
 
     /**
      * Перед каждый тестом открываем приложение
      */
-//    @BeforeMethod
-//    public void startDriver() {
-//        step("Открыть приложение", (Allure.ThrowableRunnableVoid) Selenide::open);
-//    }
-//
-//    /**
-//     * После каждого теста закрываем AndroidDriver чтобы тест атомарным был
-//     */
-//    @AfterMethod
-//    public void afterEach() {
-//        step("Закрыть приложение", Selenide::closeWebDriver);
-//    }
+    @BeforeMethod
+    public void startDriver() {
+        step("Открыть приложение", (Allure.ThrowableRunnableVoid) Selenide::open);
+    }
+
+    /**
+     * После каждого теста закрываем AndroidDriver чтобы тест атомарным был
+     */
+    @AfterMethod
+    public void afterEach() {
+        step("Закрыть приложение", Selenide::closeWebDriver);
+    }
 }
 
 /**
