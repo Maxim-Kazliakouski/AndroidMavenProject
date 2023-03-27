0f66ed1c1b7c8b420c28c9df9415c8622d5fd2ee
Maxim-Kazliakouski <maxim.kazliakouski@gmail.com>
2023-03-27 11:53:02 +0300

    feat(): adding steps

--- src/test/java/steps/LoginPageSteps.java
+++ src/test/java/steps/LoginPageSteps.java
@@ -1,11 +1,9 @@
 package steps;
 
-import com.codeborne.selenide.Condition;
-import io.appium.java_client.MobileBy;
+import io.qameta.allure.Step;
 import lombok.extern.log4j.Log4j2;
 import pages.LoginPage;
 
-import static com.codeborne.selenide.Selenide.$;
 @Log4j2
 public class LoginPageSteps {
     LoginPage loginPage;
@@ -14,6 +12,7 @@ public class LoginPageSteps {
         loginPage = new LoginPage();
     }
 
+    @Step("Check that Login page is opened")
     public LoginPageSteps loginPageOpened() {
         loginPage
                 .isOpened();
@@ -21,18 +20,21 @@ public class LoginPageSteps {
         return this;
     }
 
+    @Step("Login user")
     public void userLogged() {
         loginPage
                 .login();
         log.info("User is logged");
     }
 
-    public void isUserLogged(String getText){
+    @Step("Check, that user is logged")
+    public void isUserLogged(String getText) {
         loginPage
                 .isUserLogged(getText);
     }
 
-    public LoginPageSteps userLoggedWithDifferCreds(String username){
+    @Step("Check user logging with the username --> {username}")
+    public LoginPageSteps userLoggedWithDifferCreds(String username) {
         loginPage
                 .loginWithDifferCreds(username);
         return this;
