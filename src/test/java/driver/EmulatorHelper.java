bbcb3297c0a5398d69ed26f49a7c2edcf8e600a1
Maxim-Kazliakouski <maxim.kazliakouski@gmail.com>
2023-03-27 11:50:07 +0300

    feat(): adding function for swiping screen

--- src/test/java/driver/EmulatorHelper.java
+++ src/test/java/driver/EmulatorHelper.java
@@ -7,6 +7,7 @@ import io.appium.java_client.android.nativekey.AndroidKey;
 import io.appium.java_client.android.nativekey.KeyEvent;
 import io.appium.java_client.touch.WaitOptions;
 import io.appium.java_client.touch.offset.PointOption;
+import org.openqa.selenium.Dimension;
 
 import java.time.Duration;
 
@@ -62,5 +63,69 @@ public class EmulatorHelper extends EmulatorDriver{
     public static void sendKeysAndFind(SelenideElement element, String text){
         element.sendKeys(text);
         driver.pressKey(new KeyEvent(AndroidKey.ENTER));
+    }
+    /**
+     * Performs small swipe from the center of screen
+     *
+     * @param dir the direction of swipe
+     * @version java-client: 7.3.0
+     **/
+
+    public static void swipeScreen(String dir, int swipeDivider) {
+//        System.out.println("swipeScreenSmall(): dir: '" + dir + "'"); // always log your actions
+
+        // Animation default time:
+        //  - Android: 300 ms
+        //  - iOS: 200 ms
+        // final value depends on your app and could be greater
+        final int ANIMATION_TIME = 200; // ms
+
+        final int PRESS_TIME = 200; // ms
+
+        PointOption pointOptionStart, pointOptionEnd;
+
+        // init screen variables
+        Dimension dims = driver.manage().window().getSize();
+
+        // init start point = center of screen
+        pointOptionStart = PointOption.point(dims.width / 2, dims.height / 2);
+
+        // reduce swipe move into multiplier times comparing to swipeScreen move
+        switch (dir) {
+            case "DOWN": // center of footer
+                pointOptionEnd = PointOption.point(dims.width / 2, (dims.height / 2) + (dims.height / 2) / swipeDivider);
+                break;
+            case "UP": // center of header
+                pointOptionEnd = PointOption.point(dims.width / 2, (dims.height / 2) - (dims.height / 2) / swipeDivider);
+                break;
+            case "LEFT": // center of left side
+                pointOptionEnd = PointOption.point((dims.width / 2) - (dims.width / 2) / swipeDivider, dims.height / 2);
+                break;
+            case "RIGHT": // center of right side
+                pointOptionEnd = PointOption.point((dims.width / 2) + (dims.width / 2) / swipeDivider, dims.height / 2);
+                break;
+            default:
+                throw new IllegalArgumentException("swipeScreenSmall(): dir: '" + dir.toString() + "' NOT supported");
+        }
+
+        // execute swipe using TouchAction
+        try {
+            new TouchAction(driver)
+                    .press(pointOptionStart)
+                    // a bit more reliable when we add small wait
+                    .waitAction(WaitOptions.waitOptions(Duration.ofMillis(PRESS_TIME)))
+                    .moveTo(pointOptionEnd)
+                    .release().perform();
+        } catch (Exception e) {
+//            System.err.println("swipeScreenSmall(): TouchAction FAILED\n" + e.getMessage());
+            return;
+        }
+
+        // always allow swipe action to complete
+        try {
+            Thread.sleep(ANIMATION_TIME);
+        } catch (InterruptedException e) {
+            // ignore
+        }
     }
 }
