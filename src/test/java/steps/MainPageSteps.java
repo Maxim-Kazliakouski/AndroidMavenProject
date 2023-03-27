b71913e78e191f7a4f8da4890c2576b59b51e82c
Maxim-Kazliakouski <maxim.kazliakouski@gmail.com>
2023-03-27 11:55:30 +0300

    feat(): adding steps

--- src/test/java/steps/MainPageSteps.java
+++ src/test/java/steps/MainPageSteps.java
@@ -1,5 +1,6 @@
 package steps;
 
+import io.qameta.allure.Step;
 import pages.LoginPage;
 import pages.MainPage;
 
@@ -12,15 +13,42 @@ public class MainPageSteps {
         loginPage = new LoginPage();
     }
 
+    @Step("Check, that main page is opened!")
     public MainPageSteps mainPageOpened() {
         mainPage.
                 isOpened();
         return this;
     }
 
-    public MainPageSteps addGoodInCart(String goodName){
+    @Step("Adding item --> '{goodName}' into the cart")
+    public MainPageSteps addGoodInCart(String goodName) {
         mainPage
                 .addingGoodInCart(goodName);
         return this;
+    }
+
+    @Step("Check, that number of added items displays correct on the cart icon")
+    public MainPageSteps checkingNumberOnTheCart(Integer numberOfAddedItems) {
+        mainPage
+                .isNumberOnTheCartIconEqualsAddedItems(numberOfAddedItems);
+        return this;
+    }
+
+    @Step("Check, that 'Add to cart' button is changed to 'Remove' button")
+    public MainPageSteps checkingButton(String goodName) {
+        mainPage
+                .isAddToCartButtonChanged(goodName);
+        return this;
+    }
+
+    @Step("Check, that sorting by Java equals App sorting")
+    public void sorting(String whatSort, String sortType) {
+        mainPage
+                .sortingByJava(whatSort);
+        mainPage
+                .chooseSorting(sortType)
+                .gettingAllItemsFromThePage(whatSort);
+        mainPage
+                .sortingByAppsEqualsJava(whatSort);
     }
 }
